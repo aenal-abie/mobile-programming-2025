a70945faf48e0dd5c3c8df3ed3cea797a172155d
Zaenal <zaenal@getsurvey.id>
2025-03-23 06:33:07 +0800

    remove

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,4 +1,3 @@
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Center } from "@/components/ui/center";
 import { Divider } from "@/components/ui/divider";
 import { Heading } from "@/components/ui/heading";
